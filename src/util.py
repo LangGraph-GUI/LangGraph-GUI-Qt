d0dff70fa18c5daf2b028c40bfe3693322ae04f1
Homun Mage <homun@posetmage.com>
2025-01-28 06:11:20 +0800

    better log

--- src/util.py
+++ src/util.py
@@ -2,5 +2,10 @@ import sys
 import os
 
 def flush_print(*args, **kwargs):
-    print(*args, **kwargs)
+    output = ""
+    for arg in args:
+      output += str(arg)
+    
+    output = output.replace("\n", "\\n")
+    print(output, **kwargs)
     sys.stdout.flush()
